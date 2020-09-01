64c8a1e7782932a8bb1339d2aa575aa2a7ea3861
Weslley <Weslleyalvesv13@gmail.com>
2020-08-31 21:13:44 -0300

    Alteração do display name de disciplina

--- src/CPSI.Negocio/Modelo/Disciplina.cs
+++ src/CPSI.Negocio/Modelo/Disciplina.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace CPSI.Negocio.Modelo
 {
     public class Disciplina : Entidade
     {
+        [DisplayName("Disciplina")] 
         public string Nome { get; set; }
         public Disciplina()
         {
