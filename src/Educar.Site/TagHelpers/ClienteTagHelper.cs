0ad8830048eac600d0693e22931ebc7d5da2e0c8
Weslley <Weslleyalvesv13@gmail.com>
2020-10-26 21:07:26 -0300

    Implementação de taghelpers de descrição do cliente

---
+++ src/Educar.Site/TagHelpers/ClienteTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Educar.Site.TagHelpers
+{
+    public class ClienteTagHelper : TagHelper
+    {
+        private readonly IConfiguration configuration; 
+        public ClienteTagHelper(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            TagHelperAttribute attribute;
+            string cliente = "";
+            string tag = "p";
+
+            //O parametro "tag" da tagHelper Clinte é responsável por definir como que tag o nome do cliente será renderizado
+            if (context.AllAttributes.TryGetAttribute("tag", out attribute)) tag = attribute.Value.ToString();
+
+            cliente = configuration.GetValue<string>("Informacoes:Cliente");
+            output.TagName = tag;
+            output.Content.SetContent(cliente);
+
+            //Removendo atributos da saída, para abastrair lógica do frontend
+            output.Attributes.RemoveAll("tag");
+        }
+    }
+}
---
+++ src/Educar.Site/TagHelpers/DescricaoClienteTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Educar.Site.TagHelpers
+{
+    public class DescricaoClienteTagHelper : TagHelper
+    {
+        private readonly IConfiguration configuration; 
+        public DescricaoClienteTagHelper(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            TagHelperAttribute attribute;
+            string cliente = "";
+            string tag = "p";
+
+            //O parametro "tag" da tagHelper DescricaoCliente é responsável por definir com que tag a descrição do cliente será renderizado
+            if (context.AllAttributes.TryGetAttribute("tag", out attribute)) tag = attribute.Value.ToString(); 
+
+            cliente = configuration.GetValue<string>("Informacoes:DescricaoCliente");
+            output.TagName = tag;
+            output.Content.SetContent(cliente);
+
+            //Removendo atributos da saída, para abastrair lógica no frontend
+            output.Attributes.RemoveAll("tag");
+        }
+    }
+}
